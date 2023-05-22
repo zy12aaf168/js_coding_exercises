ec2d2c113fe3403116d1148582dfabe973ba0b85
Zulfu <zulfu.yilmaz.a@gmail.com>
2023-05-22 16:46:04 +0100

    changed var to let

--- challenges/exercise007-optional.js
+++ challenges/exercise007-optional.js
@@ -63,7 +63,7 @@ export const createRange = (start, end, step) => {
 export const getScreentimeAlertList = (users, date) => {
 	if (users === undefined) throw new Error('users is required');
 	if (date === undefined) throw new Error('date is required');
-	var filtered = 
+	let filtered = 
 		users
 			.filter(user => user.screenTime.some(st => st.date === date && (Object.values(st.usage).reduce((a,n) => a+n) > 100)))
 			.map(user => user.username)
